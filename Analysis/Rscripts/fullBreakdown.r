d07b8db42977d0d282522202ca60fb02a6d0d3ac
TimFanelle <pychocow.com@gmail.com>
2021-12-08 12:07:32 -0800

    Updating files

--- Analysis/Rscripts/fullBreakdown.r
+++ Analysis/Rscripts/fullBreakdown.r
@@ -2,8 +2,8 @@ library(dplyr)
 library(data.table)
 setwd("/media/djtface/Chonky/Classes of milk/BME504/finalProject/AllDataCSV") #this should be changed based on the file system
 
-inputFile <- "allSoftFingerexcXYdifDist4cm.csv" #this should be changed for each finger
-outputFile <- "4cmPlots.pdf"
+inputFile <- "allSoftFingerexcXYdifDist1cm.csv" #this should be changed for each finger
+outputFile <- "1cmPlots.pdf"
 
 data <- read.csv(inputFile)
 #print(data)
@@ -29,43 +29,43 @@ pdf(outputFile)
 #Based on Distance
 exc1_dist <- select(exc1, 'EX.T1', 'Dist')
 #print(exc1_dist)
-plot(exc1_dist$EX.T1, exc1_dist$Dist)
+plot(exc1_dist$EX.T1, exc1_dist$Dist, main="Tendon 1 Excursion VS Error Distance", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc2_dist <- select(exc2, 'EX.T2', 'Dist')
 #print(exc2_dist)
-plot(exc2_dist$EX.T2, exc2_dist$Dist)
+plot(exc2_dist$EX.T2, exc2_dist$Dist, main="Tendon 2 Excursion VS Error Distance", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc3_dist <- select(exc3, 'EX.T3', 'Dist')
 #print(exc3_dist)
-plot(exc3_dist$EX.T3, exc3_dist$Dist)
+plot(exc3_dist$EX.T3, exc3_dist$Dist, main="Tendon 3 Excursion VS Error Distance", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc4_dist <- select(exc4, 'EX.T4', 'Dist')
 #print(exc4_dist)
-plot(exc4_dist$EX.T4, exc4_dist$Dist)
+plot(exc4_dist$EX.T4, exc4_dist$Dist, main="Tendon 4 Excursion VS Error Distance", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 
 #Based on dX
 exc1_dist <- select(exc1, 'EX.T1', 'Dif.X')
 #print(exc1_dist)
-plot(exc1_dist$EX.T1, exc1_dist$Dif.X)
+plot(exc1_dist$EX.T1, exc1_dist$Dif.X, main="Tendon 1 Excursion VS Error In X Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc2_dist <- select(exc2, 'EX.T2', 'Dif.X')
 #print(exc2_dist)
-plot(exc2_dist$EX.T2, exc2_dist$Dif.X)
+plot(exc2_dist$EX.T2, exc2_dist$Dif.X, main="Tendon 2 Excursion VS Error In X Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc3_dist <- select(exc3, 'EX.T3', 'Dif.X')
 #print(exc3_dist)
-plot(exc3_dist$EX.T3, exc3_dist$Dif.X)
+plot(exc3_dist$EX.T3, exc3_dist$Dif.X, main="Tendon 3 Excursion VS Error In X Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc4_dist <- select(exc4, 'EX.T4', 'Dif.X')
 #print(exc4_dist)
-plot(exc4_dist$EX.T4, exc4_dist$Dif.X)
+plot(exc4_dist$EX.T4, exc4_dist$Dif.X, main="Tendon 4 Excursion VS Error In X Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 
 #Based on DY
 exc1_dist <- select(exc1, 'EX.T1', 'Dif.Y')
 #print(exc1_dist)
-plot(exc1_dist$EX.T1, exc1_dist$Dif.Y)
+plot(exc1_dist$EX.T1, exc1_dist$Dif.Y, main="Tendon 1 Excursion VS Error In Y Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc2_dist <- select(exc2, 'EX.T2', 'Dif.Y')
 #print(exc2_dist)
-plot(exc2_dist$EX.T2, exc2_dist$Dif.Y)
+plot(exc2_dist$EX.T2, exc2_dist$Dif.Y, main="Tendon 2 Excursion VS Error In Y Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc3_dist <- select(exc3, 'EX.T3', 'Dif.Y')
 #print(exc3_dist)
-plot(exc3_dist$EX.T3, exc3_dist$Dif.Y)
+plot(exc3_dist$EX.T3, exc3_dist$Dif.Y, main="Tendon 3 Excursion VS Error In Y Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 exc4_dist <- select(exc4, 'EX.T4', 'Dif.Y')
 #print(exc4_dist)
-plot(exc4_dist$EX.T4, exc4_dist$Dif.Y)
+plot(exc4_dist$EX.T4, exc4_dist$Dif.Y, main="Tendon 4 Excursion VS Error In Y Value", xlab = "Tendon Excursion (mm)", ylab = "Error in mm")
 
 dev.off()
